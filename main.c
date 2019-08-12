1312981f3261f296cd301e9a8d467d070aabd6bb
JCAJR <zeclaudio_jr@yahoo.com.br>
2019-08-12 14:42:18 -0300

    Primeiro comentário

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void mostrar_menu(void){
+
+    system("clear");
+    printf("=======SIG INVENTORY: Controle de estoque=======\n");
+    printf(" 01 - Cadastro de produtos\n");
+    printf(" 02 - Cadastro de clientes\n");
+    printf(" 03 - Resistro de vendas\n");
+    printf(" 04 - Relatório de movimentações\n");
+    printf(" 05 - Quem somos\n");
+    printf(" 06 - Sair\n");
+
+
+}
