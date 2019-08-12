017fc1d36745e441d1726f79c2188e431fd787a0
JCAJR <zeclaudio_jr@yahoo.com.br>
2019-08-12 15:42:23 -0300

    Menu registro de movimentações

--- main.c
+++ main.c
@@ -7,12 +7,13 @@ void mostrar_menu(void){
 
     system("clear");
     printf("=======SIG INVENTORY: Controle de estoque=======\n");
-    printf(" 01 - Cadastro de produtos\n");
-    printf(" 02 - Cadastro de clientes\n");
-    printf(" 03 - Resistro de vendas\n");
-    printf(" 04 - Relatório de movimentações\n");
+    printf(" 01 - Cadastro de produtos: \n");
+    printf(" 02 - Cadastro de clientes: \n");
+    printf(" 03 - Resistro de vendas: \n");
+    printf(" 04 - Relatório de movimentações: \n");
     printf(" 05 - Quem somos\n");
     printf(" 06 - Sair\n");
+    printf("Digite a opção desejada: \n");
 
 
 }
@@ -20,11 +21,11 @@ void mostrar_menu(void){
 void menu_cadasProd(void){
 
     system("clear");
-    printf("Digite o nome do produto\n");
-    printf("Digite o marca do produto\n");
-    printf("Digite a quantidade do produto\n");
-    printf("Digite a cor do produto\n");
-    printf("Digite o tamanho do produto\n");
+    printf("Digite o nome do produto: \n");
+    printf("Digite o marca do produto: \n");
+    printf("Digite a quantidade do produto: \n");
+    printf("Digite a cor do produto: \n");
+    printf("Digite o tamanho do produto: \n");
     printf(" \n");
     printf(" \n");
 
@@ -34,10 +35,10 @@ void menu_cadasProd(void){
 void menu_cadasClie(void){
 
     system("clear");
-    printf("Digite o nome do cliente\n");
-    printf("Digite o CPF do cliente\n");
-    printf("Digite o número de contato do cliente\n");
-    printf("Digite o e-mail do cliente\n");
+    printf("Digite o nome do cliente: \n");
+    printf("Digite o CPF do cliente: \n");
+    printf("Digite o número de contato do cliente: \n");
+    printf("Digite o e-mail do cliente: \n");
     printf(" \n");
     printf(" \n");
     printf(" \n");
@@ -48,7 +49,21 @@ void menu_cadasClie(void){
 void menu_regVendas(void){
 
     system("clear");
-    printf("Digite a data desejada\n");
+    printf("Digite a data desejada: \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+
+
+}
+
+void menu_regMov(void){
+
+    system("clear");
+    printf("Digite o período desejado: \n");
     printf(" \n");
     printf(" \n");
     printf(" \n");
