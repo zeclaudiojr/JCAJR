f8dd7123f1464b6b9393d84ebdc5c033b5c95053
JCAJR <zeclaudio_jr@yahoo.com.br>
2019-08-12 15:02:10 -0300

    Telas

--- main.c
+++ main.c
@@ -16,3 +16,17 @@ void mostrar_menu(void){
 
 
 }
+
+void menu_cadastr(void){
+
+    system("clear");
+    printf("=======SIG INVENTORY: Controle de estoque=======\n");
+    printf(" 01 - Cadastro de produtos\n");
+    printf(" 02 - Cadastro de clientes\n");
+    printf(" 03 - Resistro de vendas\n");
+    printf(" 04 - Relatório de movimentações\n");
+    printf(" 05 - Quem somos\n");
+    printf(" 06 - Sair\n");
+
+
+}
